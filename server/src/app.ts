c4f6ccd92659d608f5c8d227d075ba4392d19e02
Jesstin Swadley <contact@jesstin.com>
2025-02-27 18:27:30 -0800

    typescript express server

---
+++ server/src/app.ts
@@ -0,0 +1,9 @@
+import express, { Express } from 'express';
+
+const app: Express = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+	console.log(`Server is running on Port: ${PORT}`);
+});
