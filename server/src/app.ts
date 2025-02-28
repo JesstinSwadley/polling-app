2f19a58e5b264c84322a03f547da49177c0b00cf
Jesstin Swadley <contact@jesstin.com>
2025-02-27 19:40:31 -0800

    enable cors, add post poll route, collect user data

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,17 @@
-import express, { Express } from 'express';
+import express, { Express, Request, Response } from 'express';
+import cors from 'cors';
 
 const app: Express = express();
+app.use(cors<Request>());
+app.use(express.json());
+
+app.post("/poll", (req: Request, res: Response) => {
+	let { pollTitle } = req.body;
+
+	console.log(pollTitle);
+
+	res.send("Hello from POST Poll").status(201);
+});
 
 const PORT = process.env.PORT || 3000;
 
