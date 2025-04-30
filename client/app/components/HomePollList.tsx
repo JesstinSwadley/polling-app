0911fa85483387c59ff16c096479f5b4ff76943c
Jesstin Swadley <contact@jesstin.com>
2025-04-29 20:02:35 -0700

    display polls from backend response

--- client/app/components/HomePollList.tsx
+++ client/app/components/HomePollList.tsx
@@ -1,20 +1,18 @@
 import React from 'react'
 
-const HomePollList = () => {
-	const Polls = [
-		{
-			name: "Poll 1",
-			id: 1
-		},
-		{
-			name: "Poll 2",
-			id: 2
-		}
-	]
+type Poll = {
+	id: number,
+	question: string 
+}
+
+const HomePollList = async () => {
+	const res = await fetch(`${process.env.NODE_API}/polls/get-all`);
+
+	const polls: Poll[] = await res.json();
 
 	return (
 		<div>
-			{Polls.map(poll => <li key={poll.id}><span>{poll.name}</span></li>)}
+			{polls.map(poll => <li key={poll.id}><span>{poll.question}</span></li>)}
 		</div>
 	)
 }
