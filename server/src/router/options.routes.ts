1d93c6a031790c2059419fea49cc0dc63bea14fe
Jesstin Swadley <contact@jesstin.com>
2025-05-21 21:20:03 -0700

    collect option data

--- server/src/router/options.routes.ts
+++ server/src/router/options.routes.ts
@@ -3,5 +3,11 @@ import { Request, Response, Router } from "express";
 export const optionRouter: Router = Router();
 
 optionRouter.post("/create", (req: Request, res: Response) => {
+	const { pollId, pollOption } = req.body;
+
+	if (!pollId || !pollOption) {
+		res.sendStatus(400);
+	}
+
 	res.status(201).send("Option was created");
 });
