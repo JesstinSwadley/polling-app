f4fe0df42302a7b229a1a591505c236b2c7656de
Jesstin Swadley <contact@jesstin.com>
2025-05-22 20:50:09 -0700

    store option in database

--- server/src/db/schema.ts
+++ server/src/db/schema.ts
@@ -4,3 +4,9 @@ export const polls = pgTable("polls", {
 	id: integer().primaryKey().generatedAlwaysAsIdentity(),
 	question: text('question').notNull()
 });
+
+export const options = pgTable("options", {
+	id: integer().primaryKey().generatedAlwaysAsIdentity(),
+	value: text('value').notNull(),
+	poll_id: integer('poll_id').references(() => polls.id, {onDelete: 'cascade'}).notNull()
+});
--- server/src/router/options.routes.ts
+++ server/src/router/options.routes.ts
@@ -1,13 +1,25 @@
 import { Request, Response, Router } from "express";
+import { db } from "../db/drizzle";
+import { options } from "../db/schema";
 
 export const optionRouter: Router = Router();
 
-optionRouter.post("/create", (req: Request, res: Response) => {
-	const { pollId, pollOption } = req.body;
+optionRouter.post("/create", async (req: Request, res: Response) => {
+	const { pollId, optionValue } = req.body;
 
-	if (!pollId || !pollOption) {
+	if (!pollId || !optionValue) {
 		res.sendStatus(400);
 	}
+
+	await db
+		.insert(options)
+		.values({
+			poll_id: pollId,
+			value: optionValue
+		})
+		.returning({
+			id: options.id
+		});
 
 	res.status(201).send("Option was created");
 });
