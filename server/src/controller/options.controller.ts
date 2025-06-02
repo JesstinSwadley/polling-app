4c168922ef9672b8ab5c644154719cd78c665c39
Jesstin Swadley <contact@jesstin.com>
2025-06-01 23:56:17 -0700

    get options for polls

--- server/src/controller/options.controller.ts
+++ server/src/controller/options.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { db } from "../db/drizzle";
-import { options } from "../db/schema";
+import { options, polls } from "../db/schema";
+import { eq } from "drizzle-orm";
 
 export const createOptionController = async (req: Request, res: Response) => {
 	try {
@@ -21,6 +22,24 @@ export const createOptionController = async (req: Request, res: Response) => {
 			});
 	
 		res.status(201).send("Option was created");
+	} catch (error) {
+		console.error(error);
+
+		res.status(400).send();
+
+		return;
+	}
+}
+
+export const getAllPollOptionsController = async (req: Request, res: Response) => {
+	try {
+		const pollId = req.query.poll_id;
+
+		const pollOptions = await db.select().from(options).where(eq(options.poll_id, pollId));
+
+		res.status(200).send(pollOptions);
+
+		return;
 	} catch (error) {
 		console.error(error);
 
--- server/src/router/options.routes.ts
+++ server/src/router/options.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { createOptionController } from "../controller/options.controller";
+import { createOptionController, getAllPollOptionsController } from "../controller/options.controller";
 
 export const optionRouter: Router = Router();
 
 optionRouter.post("/create", createOptionController);
+optionRouter.get("/get-all", getAllPollOptionsController);
