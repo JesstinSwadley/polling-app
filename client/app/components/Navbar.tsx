b1e74b1e8757d0d85914b5de450df89d97222118
Jesstin Swadley <contact@jesstin.com>
2025-06-18 22:27:31 -0700

    basic navbar design

--- client/app/components/Navbar.tsx
+++ client/app/components/Navbar.tsx
@@ -3,20 +3,39 @@ import Link from 'next/link'
 
 const Navbar = () => {
 	return (
-		<nav>
-			<div>
-				<span>Link</span>
-				<Link href={`/polls`}>Create New Poll</Link>
-				<Link href={`/polls/update`}>Update Poll</Link>
-				<Link href={`/polls/delete`}>Delete Poll</Link>
+		<nav className="bg-gray-800">
+			<div className="max-w-screen flex flex-wrap items-center justify-between mx-auto p-4">
+				<div>
+					<span>Link</span>
+					<ul>
+						<li>
+							<Link href={`/polls`}>Create New Poll</Link>
+						</li>
+						<li>
+							<Link href={`/polls/update`}>Update Poll</Link>
+						</li>
+						<li>
+							<Link href={`/polls/delete`}>Delete Poll</Link>
+						</li>
+					</ul>
+				</div>
+				
+				<div>
+					<span>Options</span>
+					<ul>
+						<li>
+							<Link href={`/options`}>Create New Option</Link>
+						</li>
+						<li>
+							<Link href={`/options/update`}>Update Option</Link>
+						</li>
+						<li>
+							<Link href={`/options/delete`}>Delete Option</Link>
+						</li>
+					</ul>
+				</div>
 			</div>
-			
-			<div>
-				<span>Options</span>
-				<Link href={`/options`}>Create New Option</Link>
-				<Link href={`/options/update`}>Update Option</Link>
-				<Link href={`/options/delete`}>Delete Option</Link>
-			</div>
+
 		</nav>
 	)
 }
