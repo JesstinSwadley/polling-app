5d583ec8f2dc69b172c848ae1fa31678e7f25447
Jesstin Swadley <contact@jesstin.com>
2025-11-12 00:12:08 -0800

    login a user route & controller

--- server/src/controller/v1/auth.controller.ts
+++ server/src/controller/v1/auth.controller.ts
@@ -1,7 +1,8 @@
 import { type Request, type Response } from "express";
-import { genSalt, hash } from "bcryptjs";
+import { genSalt, hash, compare } from "bcryptjs";
 import { db } from "../../db/drizzle";
 import { users } from "../../db/schema";
+import { eq } from "drizzle-orm";
 
 export const registerUserController = async (req: Request, res: Response) => {
 	try {
@@ -25,6 +26,37 @@ export const registerUserController = async (req: Request, res: Response) => {
 			});
 
 		res.status(201).send(`User has been registered`);
+	} catch (err) {
+		console.error(err);
+
+		res.status(400).send(err);
+
+		return;
+	}
+}
+
+export const loginUserController = async (req: Request, res: Response) => {
+	try {
+		const { username, password } = req.body;
+
+		if (!username || !password) {
+			throw new Error("Missing Data");
+		}
+
+		const user = await db
+			.select()
+			.from(users)
+			.where(
+				eq(users.username, username)
+			);
+
+		const matchingPass = await compare(password, user[0].hash_password);
+
+		if (!matchingPass) {
+			throw new Error("Password is incorrect");
+		}
+
+		res.status(200).send("User has been logged in");
 	} catch (err) {
 		console.error(err);
 
--- server/src/router/v1/auth.routes.ts
+++ server/src/router/v1/auth.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { registerUserController } from "../../controller/v1/auth.controller";
+import { loginUserController, registerUserController } from "../../controller/v1/auth.controller";
 
 // Router
 export const authRouter: Router = Router();
 
 // Routes
 authRouter.post("/register", registerUserController);
+authRouter.post("/login", loginUserController);
